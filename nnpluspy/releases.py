4122088c2dbff4b4ca68380caf9c797bf0038e24
Daan Mathot <daanmathot@gmail.com>
2012-12-16 14:38:02 +0100

    ready for custom queries

--- nnpluspy/releases.py
+++ nnpluspy/releases.py
@@ -3,107 +3,13 @@
 # daanmathot@gmail.com
 # Wed Dec 12 22:48:54 2012
 
+import logging
 import os
 
 from nnpluspy import db
 from nnpluspy import WWW_DIR
 
-
-def delete_release(release):
-    con = db.connect()
-
-    # Remove from disk
-    nzb_base_path = con.select("SELECT value FROM site WHERE setting='nzbpath'")[0][0]
-    audio_base_path = os.path.join(WWW_DIR, 'covers', 'audio')
-    img_base_path = os.path.join(WWW_DIR, 'covers', 'preview')
-
-    if release.guid:
-        files = [
-            os.path.join(nzb_base_path, release.guid[0], release.guid + '.nzb.gz'),
-            os.path.join(audio_base_path, release.guid[0], release.guid + '.mp3'),
-            os.path.join(img_base_path, release.guid + '_thumb.jpg')
-        ]
-    else:
-        files = [
-            os.path.join(nzb_base_path, release.rid[0], release.rid + '.nzb.gz'),
-            os.path.join(audio_base_path, release.rid[0], release.rid + '.mp3')
-        ]
-
-    try:
-        os.remove(nzb_path)
-        os.remove(audio_path)
-    except IOError:
-        pass
-
-    # Remove from database
-    """
-    DELETE FROM releasefiles WHERE releaseID '%s'
-
-    DELETE FROM releasecomment WHERE releaseID '%s'
-    UPDATE releases SET comments = (select count(ID) from releasecomment WHERE
-    releasecomment.releaseID = '%d')
-    WHERE releases.ID = '%d', rid, rid
-
-    DELETE FROM (releaseaudio, releasesubs, releaseextrafull, releasevideo) releaseid
-
-
-
-
-    """
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def get_releases_by_id(release_ids, query_filter=None):
-    con = db.connect()
-    gen_keys = lambda myDict: [x + "=?" for x in myDict.keys()]
-    query = "SELECT * FROM releases WHERE id IN({})".format(
-            str(release_ids).strip('[]'))
-
-    if query_filter:
-        query += " AND " + " AND ".join(gen_keys(query_filter))
-
-        sqlResult = con.select(query, query_filter.values())
-    else:
-        sqlResult = con.select(query)
-
-    con.close_db()
-
-    return [Release(x) for x in sqlResult]
-
-
-def get_releases_by_name(release_name, query_filter=None, strict=False):
-    con = db.connect()
-    gen_keys = lambda myDict: [k + "=" + v for k, v in myDict.iteritems()]
-
-    if strict:
-        strictness = "="
-    else:
-        release_name = "%{}%".format(release_name)
-        strictness = "LIKE"
-
-    query = "SELECT * FROM releases WHERE name {strictness} %s".format(
-            strictness=strictness)
-
-    if query_filter:
-        query += " AND " + " AND ".join(gen_keys(query_filter))
-        sqlResult = con.select(query, (release_name,))
-    else:
-        sqlResult = con.select(query, (release_name,))
-
-    con.close_db()
-
-    return [Release(x) for x in sqlResult]
+logging.getLogger(__name__)
 
 
 class Release(object):
@@ -131,3 +37,99 @@ class Release(object):
         self.cid = values[COL['categoryID']]
         self.rageid = values[COL['rageID']]
         self.tvdbid = values[COL['tvdbID']]
+
+    def __repr__(self):
+        return "{rid} {name}".format(rid=self.rid, name=self.name)
+
+    def delete(self, nzb_dir=None):
+        if not nzb_dir:
+            nzb_dir = db.get_nzb_path()
+        img_dir = os.path.join(WWW_DIR, 'covers')
+        audio_dir = os.path.join(WWW_DIR, 'covers', 'audio')
+
+        if self.guid:
+            files = [
+                os.path.join(nzb_dir, self.guid[0], self.guid + '.nzb.gz'),
+                os.path.join(audio_dir, self.guid[0], self.guid + '.mp3'),
+                os.path.join(img_dir, self.guid + '_thumb.jpg')
+            ]
+        else:
+            files = [
+                os.path.join(nzb_dir, self.rid[0], self.rid + '.nzb.gz'),
+                os.path.join(audio_dir, self.rid[0], self.rid + '.mp3')
+            ]
+
+        logging.debug(files)
+        for f in files:
+            logging.debug("Removing: {file}".format(file=f))
+            try:
+                os.remove(f)
+            except OSError, e:
+                logging.debug(e)
+
+
+def delete_releases(releases):
+    cur = db.connect()
+    nzb_dir = cur.select("SELECT value FROM site WHERE setting='nzbpath'")[0][0]
+
+    for release in releases:
+        logging.debug("Deleting release: {release}".format(release=release))
+        # Remove static files
+        try:
+            release.delete(nzb_dir)
+        except OSError, e:
+            logging.debug(e)
+
+        # Remove releases' database entries
+        for table in ('releasenfo', 'releasefiles', 'usercart', 'userdownloads',
+                      'releaseaudio', 'releasesubs', 'releaseextrafull',
+                      'releasevideo', 'releasecomment', 'releases'):
+            if table == 'releases':
+                cur.delete(table, {'ID': release.rid})
+            else:
+                cur.delete(table, {'releaseID': release.rid})
+
+        # Correct comment count
+        cur.action("UPDATE releases SET comments = (SELECT count(ID) from releasecomment WHERE "
+                   "releasecomment.releaseID = %s) WHERE releases.ID = %s",
+                   (release.rid, release.rid,))
+
+    cur.close()
+
+
+def get_releases_strict(in_dict, and_dict=None):
+    cur = db.connect()
+    gen_keys = lambda myDict: [x + "=?" for x in myDict.keys()]
+    query = "SELECT * FROM releases WHERE {in_query} IN({restrict})".format(
+            in_query=in_dict.keys()[0],
+            restrict=str(in_dict.values()[0].strip('[]')))
+
+    if and_dict:
+        query += " AND " + " AND ".join(gen_keys(and_dict))
+
+        sqlResult = cur.select(query, and_dict.values())
+    else:
+        sqlResult = cur.select(query)
+
+    cur.close()
+
+    return [Release(x) for x in sqlResult]
+
+
+def get_releases_filter(where_dict, and_dict=None):
+    cur = db.connect()
+    gen_keys = lambda myDict: [k + "=" + v for k, v in myDict.iteritems()]
+
+    # 'Encapsulate' query for fuzzy search
+    query = "SELECT * FROM releases WHERE {like_query} LIKE {restrict}".format(
+            like_query=where_dict.keys()[0],
+            restrict="'%{}%'".format(where_dict.values()[0]))
+
+    if and_dict:
+        query += " AND " + " AND ".join(gen_keys(and_dict))
+
+    sqlResult = cur.select(query)
+
+    cur.close()
+
+    return [Release(x) for x in sqlResult]
