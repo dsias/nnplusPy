f4cec5ec87af2437c0514be656d608bcdc672a96
Daan Mathot <daanmathot@gmail.com>
2012-12-16 14:38:26 +0100

    nm

--- nnpluspy/__init__.py
+++ nnpluspy/__init__.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 # encoding: utf-8
 # daanmathot@gmail.com
-# Wed Dec 12 21:52:32 2012
+# Wed Dec 12 18:45:23 2012
+
+import logging
+
+# the www dir has to be specified before we can begin:
+WWW_DIR = '/srv/http/nnplus/www'
+
+# Initialize the log
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(module)-8s :: %(message)s',
+    datefmt='%H:%M:%S')
 
 
-NNPLUS_WWW = '/srv/http/nnplus/www'
+#NZB_DIR = get_nzb_path()
+#AUDIO_DIR = os.path.join(WWW_DIR, 'covers', 'audio')
+#IMG_DIR = os.path.join(WWW_DIR, 'covers', 'preview')
