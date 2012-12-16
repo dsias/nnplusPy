b55dc18c5791b6259c893d2a2474e3174ac86b29
Daan Mathot <daanmathot@gmail.com>
2012-12-16 14:37:37 +0100

    makes a bit more sense now

--- nnpluspy/db.py
+++ nnpluspy/db.py
@@ -3,14 +3,20 @@
 # daanmathot@gmail.com
 # Tue Dec 11 22:44:47 2012
 
-
 import MySQLdb
+import logging
 
-from nnpluspy.nnplus_config import get_settings
+
+from nnpluspy.utils import get_config_php
+
+
+logging.getLogger(__name__)
 
 
 class DBConnection():
     def __init__(self, db_password, db_user, db_name, host='archsrv'):
+        logging.info("Connecting to the database: {db_user}' @ '{db_name}'".format(
+            db_user=db_user, db_name=db_name))
 
         if host:
             self.con = MySQLdb.connect(user=db_user,
@@ -24,7 +30,8 @@ class DBConnection():
 
         self.cur = self.con.cursor()
 
-    def action(self, query, args=None):
+    def action(self, query, args=None, dry_run=False):
+        logging.info("Query: {query} {args}".format(query=query, args=args))
 
         if args is None:
             self.cur.execute(query)
@@ -34,10 +41,19 @@ class DBConnection():
         return self.cur
 
     def select(self, query, args=None):
+        logging.debug(query)
         self.cur.execute(query, args)
         sqlResult = self.cur.fetchall()
 
         # Unpack results
+        return sqlResult
+
+    def delete(self, table, where_dict):
+        gen_params = lambda myDict: [x + "=%s" for x in myDict.keys()]
+        query = "DELETE FROM " + table + " WHERE " + " AND ".join(gen_params(where_dict))
+
+        sqlResult = self.action(query, where_dict.values())
+
         return sqlResult
 
     def update(self, table, value_dict, key_dict):
@@ -48,19 +64,12 @@ class DBConnection():
 
         return sqlResult
 
-    def close_db(self):
+    def close(self):
+        logging.info("Closed connection to database.")
         self.con.close()
 
 
 def connect():
+    con = DBConnection(**get_config_php(['DB_PASSWORD', 'DB_USER', 'DB_NAME']))
 
-    db = DBConnection(**get_settings(['DB_PASSWORD', 'DB_USER', 'DB_NAME']))
-
-    return db
-
-
-def close_db(db):
-    try:
-        db.con.close()
-    except MySQLdb.OperationalError:
-        pass
+    return con
